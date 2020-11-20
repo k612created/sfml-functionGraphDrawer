3e0426efa74b38115381e884846844062699a99b
Sam Huang <63796156+k612created@users.noreply.github.com>
2020-11-20 11:58:45 +0800

    Update simpleFunction.hpp

--- include/simpleFunction.hpp
+++ include/simpleFunction.hpp
@@ -1,16 +1,24 @@
 #ifndef SFML_FGD_SIMPLEFUNCTION_HPP
 #define SFML_FGD_SIMPLEFUNCTION_HPP
+
+#include <exception>
 using namespace std;
 
+struct FunctionError : public exception {
+	const char * what() const throw() {
+		return "Error calculating the function.";
+	}
+};
+
 class SimpleFunction {
-public:
-	double k, b; // For linear functions
+	public:
+	double k, b; // For linear functions and inverse proportional functions
 	double a, c; // For quadratic functions
 	virtual double y(double x);
 };
 
 class LinearFunction : public SimpleFunction {
-public:
+	public:
 	LinearFunction(double K, double B) {
 		k = K;
 		b = B;
@@ -20,6 +28,28 @@ public:
 	}
 };
 
-class 
+class InversePropFunction : public SimpleFunction {
+	public:
+	InversePropFunction(double K) {
+		k = K;
+	}
+	double y(double x) {
+		if (x == 0) {
+			throw FunctionError();
+		} else return k / x;
+	}
+};
+
+class QuadraticFunction : public SimpleFunction {
+	public:
+	QuadraticFunction(double A, double B, double C) {
+		a = A;
+		b = B;
+		c = C;
+	}
+	double y(double x) {
+		return a * x * x + b * x + c;
+	}
+};
 
 #endif
